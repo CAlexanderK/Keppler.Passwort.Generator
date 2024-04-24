288a91f56a8bbd7482da6b1681a25b82f1b1e255
CAlexanderK <146945910+CAlexanderK@users.noreply.github.com>
2024-04-24 10:55:15 +0200

    Projektdateien hinzufügen.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Keppler.Passwort.Generator
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
