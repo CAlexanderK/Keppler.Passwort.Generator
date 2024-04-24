d0d44fda1f8c6556d931f1e4c494caffb7d51035
CAlexanderK <146945910+CAlexanderK@users.noreply.github.com>
2024-04-24 23:05:51 +0200

    93 Abfragen erstellt, um von einer Randomzahl über ASCII-Tabelle auf ein einzelnes Zeichen zu kommen. 2 Sonderzeichen noch überarbeiten

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -21,12 +21,403 @@ namespace Keppler.Passwort.Generator
         public bool _letterSmallTrueOrFalse;
         public bool _specialCharacterTrueOrFalse;
 
-        public int _number;
-        public string _character;
+        public int _number = 0;
+        public string _character = "";
 
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        public string GeneratingPasswordCharacterASCII(int number) 
+        {
+            // Anfang der Abfragen mit den Zahlen
+            if (number == 48)
+            {
+                return "0";
+            }
+            else if (number == 49)
+            {
+                return "1";
+            }
+            else if (number == 50)
+            {
+                return "2";
+            }
+            else if (number == 51)
+            {
+                return "3";
+            }
+            else if (number == 52)
+            {
+                return "4";
+            }
+            else if (number == 53)
+            {
+                return "5";
+            }
+            else if (number == 54)
+            {
+                return "6";
+            }
+            else if (number == 55)
+            {
+                return "7";
+            }
+            else if (number == 56)
+            {
+                return "8";
+            }
+            else if (number == 57)
+            {
+                return "9";
+            }
+            // Ende Zahlen Anfang Großbuchstaben
+            else if (number == 65)
+            {
+                return "A";
+            }
+            else if (number == 66)
+            {
+                return "B";
+            }
+            else if (number == 67)
+            {
+                return "C";
+            }
+            else if (number == 68)
+            {
+                return "D";
+            }
+            else if (number == 69)
+            {
+                return "E";
+            }
+            else if (number == 70)
+            {
+                return "F";
+            }
+            else if (number == 71)
+            {
+                return "G";
+            }
+            else if (number == 72)
+            {
+                return "H";
+            }
+            else if (number == 73)
+            {
+                return "I";
+            }
+            else if (number == 74)
+            {
+                return "J";
+            }
+            else if (number == 75)
+            {
+                return "K";
+            }
+            else if (number == 76)
+            {
+                return "L";
+            }
+            else if (number == 77)
+            {
+                return "M";
+            }
+            else if (number == 78)
+            {
+                return "N";
+            }
+            else if (number == 79)
+            {
+                return "O";
+            }
+            else if (number == 80)
+            {
+                return "P";
+            }
+            else if (number == 81)
+            {
+                return "Q";
+            }
+            else if (number == 82)
+            {
+                return "R";
+            }
+            else if (number == 83)
+            {
+                return "S";
+            }
+            else if (number == 84)
+            {
+                return "T";
+            }
+            else if (number == 85)
+            {
+                return "U";
+            }
+            else if (number == 86)
+            {
+                return "V";
+            }
+            else if (number == 87)
+            {
+                return "W";
+            }
+            else if (number == 88)
+            {
+                return "X";
+            }
+            else if (number == 89)
+            {
+                return "Y";
+            }
+            else if (number == 90)
+            {
+                return "Z";
+            }
+            // Ende Großbuchstaben Anfang Kleinbuchstaben
+            else if (number == 97)
+            {
+                return "a";
+            }
+            else if (number == 98)
+            {
+                return "b";
+            }
+            else if (number == 99)
+            {
+                return "c";
+            }
+            else if (number == 100)
+            {
+                return "d";
+            }
+            else if (number == 101)
+            {
+                return "e";
+            }
+            else if (number == 102)
+            {
+                return "f";
+            }
+            else if (number == 103)
+            {
+                return "g";
+            }
+            else if (number == 104)
+            {
+                return "h";
+            }
+            else if (number == 105)
+            {
+                return "i";
+            }
+            else if (number == 106)
+            {
+                return "j";
+            }
+            else if (number == 107)
+            {
+                return "k";
+            }
+            else if (number == 108)
+            {
+                return "l";
+            }
+            else if (number == 109)
+            {
+                return "m";
+            }
+            else if (number == 110)
+            {
+                return "n";
+            }
+            else if (number == 111)
+            {
+                return "o";
+            }
+            else if (number == 112)
+            {
+                return "p";
+            }
+            else if (number == 113)
+            {
+                return "q";
+            }
+            else if (number == 114)
+            {
+                return "r";
+            }
+            else if (number == 115)
+            {
+                return "s";
+            }
+            else if (number == 116)
+            {
+                return "t";
+            }
+            else if (number == 117)
+            {
+                return "u";
+            }
+            else if (number == 118)
+            {
+                return "v";
+            }
+            else if (number == 119)
+            {
+                return "w";
+            }
+            else if (number == 120)
+            {
+                return "x";
+            }
+            else if (number == 121)
+            {
+                return "y";
+            }
+            else if (number == 122)
+            {
+                return "z";
+            }
+            // Ende Kleinbuchstaben Anfang Sonderzeichen
+            else if (number == 33)
+            {
+                return "!";
+            }
+            else if (number == 34)
+            {
+                // Fall nachfragen "
+                return "";
+            }
+            else if (number == 35)
+            {
+                return "#";
+            }
+            else if (number == 36)
+            {
+                return "$";
+            }
+            else if (number == 37)
+            {
+                return "%";
+            }
+            else if (number == 38)
+            {
+                return "&";
+            }
+            else if (number == 39)
+            {
+                return "'";
+            }
+            else if (number == 40)
+            {
+                return "(";
+            }
+            else if (number == 41)
+            {
+                return ")";
+            }
+            else if (number == 42)
+            {
+                return "*";
+            }
+            else if (number == 43)
+            {
+                return "+";
+            }
+            else if (number == 44)
+            {
+                return ",";
+            }
+            else if (number == 45)
+            {
+                return "-";
+            }
+            else if (number == 46)
+            {
+                return ".";
+            }
+            else if (number == 47)
+            {
+                return "/";
+            }
+            else if (number == 58)
+            {
+                return ":";
+            }
+            else if (number == 59)
+            {
+                return ";";
+            }
+            else if (number == 60)
+            {
+                return "<";
+            }
+            else if (number == 61)
+            {
+                return "=";
+            }
+            else if (number == 62)
+            {
+                return ">";
+            }
+            else if (number == 63)
+            {
+                return "?";
+            }
+            else if (number == 64)
+            {
+                return "@";
+            }
+            else if (number == 91)
+            {
+                return "[";
+            }
+            else if (number == 92)
+            {
+                // Fall nachfragen \
+                return "";
+            }
+            else if (number == 93)
+            {
+                return "]";
+            }
+            else if (number == 94)
+            {
+                return "^";
+            }
+            else if (number == 95)
+            {
+                return "_";
+            }
+            else if (number == 96)
+            {
+                return "`";
+            }
+            else if (number == 123)
+            {
+                return "{";
+            }
+            else if (number == 124)
+            {
+                return "|";
+            }
+            else if (number == 125)
+            {
+                return "}";
+            }
+            else if (number == 126)
+            {
+                return "~";
+            }
+            else
+            {
+                // Abschlussfall, damit alle möglichen Fälle einen Wert liefern, auch wenn dieser leer ist.
+                return "";
+            }
         }
 
         public void cbCaseOne_Checked(object sender, RoutedEventArgs e)
