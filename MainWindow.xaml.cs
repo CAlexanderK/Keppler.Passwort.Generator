c32e44e9912bf5efc64d63dacceac6498f705b86
CAlexanderK <146945910+CAlexanderK@users.noreply.github.com>
2024-04-24 16:47:49 +0200

    erste Variablen und Methoden für die CheckBoxen erstellt

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -16,9 +16,59 @@ namespace Keppler.Passwort.Generator
     /// </summary>
     public partial class MainWindow : Window
     {
+        public bool _numberTrueOrFalse;
+        public bool _letterBigTrueOrFalse;
+        public bool _letterSmallTrueOrFalse;
+        public bool _specialCharacterTrueOrFalse;
+
+        public int _number;
+        public string _character;
+
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        public void cbCaseOne_Checked(object sender, RoutedEventArgs e)
+        {
+            _numberTrueOrFalse = true;
+        }
+
+        public void cbCaseOne_Unchecked(object sender, RoutedEventArgs e)
+        {
+            _numberTrueOrFalse = false;
+        }
+
+        public void cbCaseTwo_Checked(object sender, RoutedEventArgs e)
+        {
+            _letterBigTrueOrFalse = true;
+        }
+
+        public void cbCaseTwo_Unchecked(object sender, RoutedEventArgs e)
+        {
+            _letterBigTrueOrFalse = false;
+        }
+
+        public void cbCaseThree_Checked(object sender, RoutedEventArgs e)
+        {
+            _letterSmallTrueOrFalse = true;
+        }
+
+        public void cbCaseThree_Unchecked(object sender, RoutedEventArgs e)
+        {
+            _letterSmallTrueOrFalse = false;
+        }
+
+        public void cbCaseFour_Checked(object sender, RoutedEventArgs e)
+        {
+            _specialCharacterTrueOrFalse = true;
+        }
+
+        public void cbCaseFour_Unchecked(object sender, RoutedEventArgs e)
+        {
+            _specialCharacterTrueOrFalse = false;
+        }
+
+
     }
 }
